23088360be6348c5a2950163c420504707f95170
TranHuy2608 <63567397+TranHuy2608@users.noreply.github.com>
2022-08-03 08:57:27 +0700

    Add files via upload

---
+++ sap-rfc-csharp-master/RFC_SAP_Interface/rfc_Connector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAP.Middleware.Connector;
+
+namespace RFC_SAP_Interface
+{
+    public class rfc_Connector : IDestinationConfiguration
+    {
+        public event RfcDestinationManager.ConfigurationChangeHandler ConfigurationChanged;
+
+        public bool ChangeEventsSupported()
+        {
+            return false;
+        }
+
+        public RfcConfigParameters GetParameters(string destinationName)
+        {
+            if ("SE37".Equals(destinationName))
+            {
+                
+
+                RfcConfigParameters parms = new RfcConfigParameters();
+                parms.Add(RfcConfigParameters.AppServerHost, "");
+                parms.Add(RfcConfigParameters.SystemNumber, "10");
+                parms.Add(RfcConfigParameters.SystemID, "S4I");
+                parms.Add(RfcConfigParameters.User, "");
+                parms.Add(RfcConfigParameters.Password, "");
+                parms.Add(RfcConfigParameters.Client, "");
+                parms.Add(RfcConfigParameters.SAPRouter, "");
+                parms.Add(RfcConfigParameters.Language, "EN");
+                parms.Add(RfcConfigParameters.Name, "SE37");
+                parms.Add(RfcConfigParameters.ConnectionIdleTimeout, "600");
+                parms.Add(RfcConfigParameters.PoolSize, "10");
+
+                return parms;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
